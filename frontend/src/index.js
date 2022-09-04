4fa7f38428b1a8dd6f536a61a28e2e9f3e45747a
Nilesh <ece.20becd76@silicon.ac.in>
2022-09-04 19:01:21 +0530

    []

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './bootstrap.min.css'
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -16,4 +17,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.register();
 reportWebVitals();
+
